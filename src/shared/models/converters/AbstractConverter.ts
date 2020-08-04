553a4f4772c5410e2492a8d4e54a0a44cbf3736b
Evitcani <33763283+evitcani@users.noreply.github.com>
2020-08-04 16:39:22 -0500

    Remove keys.

--- src/shared/models/converters/AbstractConverter.ts
+++ src/shared/models/converters/AbstractConverter.ts
@@ -1,17 +1,7 @@
-import { keys } from 'ts-transformer-keys';
-
 /**
  * Abstract converter.
  */
 export abstract class AbstractConverter<T extends object, U extends object> {
-    private keysOfVO: any[];
-    private keysOfDTO: any[];
-
-    constructor() {
-        this.keysOfVO = keys<T>();
-        this.keysOfDTO = keys<U>();
-    }
-
     public convertVoToDto(vo: T): U | null {
         return this.convertExistingVoToDto(vo, this.getNewDTO());
     }
