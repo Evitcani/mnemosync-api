ad501feff198f616776fe6c2608bb813a00c0b1d
Evitcani <33763283+evitcani@users.noreply.github.com>
2020-08-10 22:36:03 -0500

    Add primary columns.

--- src/backend/entity/WorldToUser.ts
+++ src/backend/entity/WorldToUser.ts
@@ -1,14 +1,14 @@
-import {Column, Entity, JoinColumn, ManyToOne} from "typeorm";
+import {Entity, JoinColumn, ManyToOne, PrimaryColumn} from "typeorm";
 import {TableName} from "../../shared/documentation/databases/TableName";
 import {ColumnName} from "../../shared/documentation/databases/ColumnName";
 import {World} from "./World";
 
 @Entity({name: TableName.WORLD_OWNERS})
 export class WorldToUser {
-    @Column("text", {name: ColumnName.DISCORD_ID})
+    @PrimaryColumn("text", {name: ColumnName.DISCORD_ID})
     discordId: string;
 
-    @Column({name: ColumnName.WORLD_ID})
+    @PrimaryColumn({name: ColumnName.WORLD_ID})
     worldId?: string;
 
     @ManyToOne(type => World, {
